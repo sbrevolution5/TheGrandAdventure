202095e059e769ab4e527dacbca903bc8a89b442
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-22 13:43:12 -0500

    Initialized dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheGrandAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
